be9126ba32c391e9e97a9b744cf0b354efec72a3
abagayev <abagayev@gmail.com>
2016-11-06 18:20:50 +0200

    Moving on, distance and time range added First prototype of storing user data

--- index.php
+++ index.php
@@ -7,21 +7,104 @@ try {
     $config = require_once __DIR__ . '/config.php';
     $bot = new \TelegramBot\Api\Client($config['bot_token'], $config['bot_tracker']);
 
-    $givemesomemoviesbutton = [
-        'text' => 'Give me some movies!',
-        'request_location' => true,
-    ];
+    $bot->on(function(\TelegramBot\Api\Types\Message $message) use ($bot, $config){
 
-    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[$givemesomemoviesbutton]], null, true);
+        // more config
 
-    $bot->on(function($message) use ($bot, $config, $keyboard){
+        $givemesomemoviesbutton = [
+            'text' => 'Give me some movies!',
+            'request_location' => true,
+        ];
 
+        $maxd = 2500;
+
+        // get info from message
+
+        $chat = $message->getChat();
         $location = $message->getLocation();
+        $text = $message->getText();
+        $state_id = $chat->getId();
 
         if (empty($location)) {
-            $bot->sendMessage($message->getChat()->getId(), 'Please give me your location first', false, null, null, $keyboard);
-            return;
+
+            // load state (if exists)
+
+            $state = @json_decode(@file_get_contents(__DIR__ . '/states/' . $state_id), 1);
+
+            $time_d = empty($state['time_d']) ? 0 : $state['time_d'];
+            $distance_d = empty($state['distance_d']) ? 1 : $state['distance_d'];
+
+            // make decision from message test
+
+            if (empty($state) || !in_array($text, ['wider', 'later', 'nearer', 'earlier'])) {
+                $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[$givemesomemoviesbutton]], null, true);
+                $bot->sendMessage($message->getChat()->getId(), 'Please give me your location first', false, null, null, $keyboard);
+                return;
+            }
+
+            // modify location and filter params
+
+            $location = new \TelegramBot\Api\Types\Location();
+            $location->setLatitude($state['location']['latitude']);
+            $location->setLongitude($state['location']['longitude']);
+
+            if ($text == 'wider') {
+                ++$distance_d;
+            }
+            elseif ($text == 'nearer') {
+                --$distance_d;
+                if ($distance_d < 1 ) $distance_d = 1;
+            }
+            elseif ($text == 'later') {
+                ++$time_d;
+            }
+            elseif ($text == 'earlier') {
+                --$time_d;
+            }
+
         }
+
+        // save state
+        // @todo move to Mongo
+
+        if (empty($state)) {
+            $state = [
+                'id' => $state_id,
+                'time' => time(),
+                'text' => $text,
+                'chat' => $chat->toJson(true),
+                'location' => $location->toJson(true),
+                'time_d' => $time_d,
+                'distance_d' => $distance_d,
+            ];
+        }
+        else {
+            $state['text'] = $text;
+            $state['time_d'] = $time_d;
+            $state['distance_d'] = $distance_d;
+        }
+
+        file_put_contents(__DIR__ . '/states/' . $state_id, json_encode($state));
+
+        // create keyboard
+
+        if (empty($text)) {
+            $buttons = [
+                ['wider', 'later'],
+                ['go back']
+            ];
+        }
+        else {
+            $buttons = [
+                ['wider', 'later'],
+                ['nearer', 'earlier'],
+                ['go back']
+            ];
+        }
+
+        $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup($buttons, null, true);
+
+        // create filters
 
         $url = $config['api_url'] . 'site/filter';
 
@@ -29,31 +112,67 @@ try {
         $time_to = clone $time_from;
         $time_to->modify('+1 hour');
 
+        if (!empty($time_d)) {
+            $time_from->modify("+{$time_d} hour");
+            $time_to->modify("+{$time_d} hour");
+        }
+
+        if ($time_to->format('H:i') < $time_from->format('H:i')) {
+            $time_to = clone $time_from;
+            $time_to->setTime(23, 59, 59);
+        }
+
+        // calculate distance
+
+        $mind = (($distance_d ?: 1) - 1) * $maxd;
+        $maxd = ($distance_d ?: 1) * $maxd;
+
+        // explain filter
+
+        $explainer =  implode(', ', [
+            'mind=' . $mind,
+            'maxd=' . $maxd,
+            'from=' . $time_from->format('H:i'),
+            'to=' . $time_to->format('H:i'),
+        ]);
+
+        $bot->sendMessage($message->getChat()->getId(), $explainer, false, null, null, $keyboard);
+
+        // build and call query
+
         $query = http_build_query([
             'time_from' => $time_from->format('H:i'),
             'time_to' => $time_to->format('H:i'),
             'position_lat' => $location->getLatitude(),
             'position_lng' => $location->getLongitude(),
-            'maxd' => 2500,
+            'mind' => $mind,
+            'maxd' => $maxd,
         ]);
 
         $str = file_get_contents("{$url}?{$query}");
         $bills = @json_decode($str, true);
+
+        // process empty bill
 
         if (empty($bills)) {
             $bot->sendMessage($message->getChat()->getId(), 'It is sad, but nothing is found. Try another location or time.', false, null, null, $keyboard);
             return;
         }
 
+        // process not empty bill
+
         $output = '';
 
         foreach ($bills as $bill) {
+
+            $time = date('H:i', strtotime($bill['time']));
+
             $output .=
-                $bill['time'] . ' ' . $bill['cinema'] . PHP_EOL .
-                $bill['movie'] . PHP_EOL . PHP_EOL;
+                "<b>$time</b> " . "<a href='kinoafisha.ua/ua/cinema/{$bill['city']['alias']}/{$bill['cinema']['alias']}'>{$bill['cinema']['title']}</a>" . PHP_EOL .
+                "<a href='http://kinoafisha.ua/ua/films/{$bill['movie']['alias']}'>{$bill['movie']['title']}</a>" . PHP_EOL . PHP_EOL;
         }
 
-        $bot->sendMessage($message->getChat()->getId(), $output);
+        $bot->sendMessage($message->getChat()->getId(), $output, 'HTML', true, null, $keyboard);
 
 
     }, function($message) use ($name) {
@@ -64,5 +183,6 @@ try {
 
 } catch (\TelegramBot\Api\Exception $e) {
     // @todo add logging
-    $e->getMessage();
+
+    error_log('TelegramBot\Api\Exception:' . $e->getMessage());
 }
